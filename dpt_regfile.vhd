d02cf7287d983787c9d6fa52a4d6838add51a0b2
Gutierrez PS <gutierrez@live.jp>
2014-11-12 21:47:56 -0300

    Regfile is back to async read

--- dpt_regfile.vhd
+++ dpt_regfile.vhd
@@ -1,11 +1,10 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
-use IEEE.STD_LOGIC_arith.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity dpt_regfile is
 port( CLK, WE3      : in STD_LOGIC;
-	  A1, A2, A3    : in STD_LOGIC_VECTOR(4 downto 0);
+	  RA1, RA2, WA3 : in STD_LOGIC_VECTOR(4 downto 0);
 	  WD3           : in STD_LOGIC_VECTOR(31 downto 0);
 	  RD1, RD2      : out STD_LOGIC_VECTOR(31 downto 0);
 	  Vdd, Vss		: in STD_LOGIC);
@@ -16,23 +15,23 @@ architecture behave of dpt_regfile is
 	signal mem: ramtype;
 
 begin
-	process(CLK) begin
-		if rising_edge(CLK) then
-            if (conv_integer(A1) = 0) then
-                RD1 <= X"00000000"; 
-            else
-                RD1 <= mem(conv_integer(A1));
-            end if;
+	process( CLK ) begin
+		if rising_edge(CLK) and WE3 = '1' then
+            mem( CONV_INTEGER(WA3) ) <= WD3;
+        end if;
+    end process;
+    
+    process( RA1, RA2, mem ) begin
+        if( CONV_INTEGER(RA1) = 0 ) then
+            RD1 <= X"00000000"; 
+        else
+            RD1 <= mem( CONV_INTEGER(RA1) );
+        end if;
 
-            if (conv_integer(A2) = 0) then
-                RD2 <= X"00000000";
-            else
-                RD2 <= mem(conv_integer(A2));
-            end if;
-
-            if WE3 = '1' then
-                mem(CONV_INTEGER(A3)) <= WD3;
-            end if;
+        if( CONV_INTEGER(RA2) = 0 ) then
+            RD2 <= X"00000000";
+        else
+            RD2 <= mem( CONV_INTEGER(RA2) );
         end if;
 	end process;
 end;
