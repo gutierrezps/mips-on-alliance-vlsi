51946e626741a3f7383ced5593321c2071c2269f
Gutierrez PS <gutierrez@live.jp>
2014-12-03 20:17:17 -0300

    Regfile pin names changed

--- dpt_regfile.vhd
+++ dpt_regfile.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity dpt_regfile is
 port( CLK, WE3      : in STD_LOGIC;
-	  RA1, RA2, WA3 : in STD_LOGIC_VECTOR(4 downto 0);
+	  A1, A2, A3    : in STD_LOGIC_VECTOR(4 downto 0);
 	  WD3           : in STD_LOGIC_VECTOR(31 downto 0);
 	  RD1, RD2      : out STD_LOGIC_VECTOR(31 downto 0);
 	  Vdd, Vss		: in STD_LOGIC);
@@ -12,26 +12,26 @@ end dpt_regfile;
 
 architecture behave of dpt_regfile is
 	type ramtype is array (31 downto 0) of STD_LOGIC_VECTOR (31 downto 0);
-	signal mem: ramtype;
+	signal registers: ramtype;
 
 begin
 	process( CLK ) begin
 		if rising_edge(CLK) and WE3 = '1' then
-            mem( CONV_INTEGER(WA3) ) <= WD3;
+            registers( CONV_INTEGER(A3) ) <= WD3;
         end if;
     end process;
     
-    process( RA1, RA2, mem ) begin
-        if( CONV_INTEGER(RA1) = 0 ) then
-            RD1 <= X"00000000"; 
+    process( A1, A2, registers ) begin
+        if( CONV_INTEGER(A1) = 0 ) then
+            RD1 <= X"00000000";
         else
-            RD1 <= mem( CONV_INTEGER(RA1) );
+            RD1 <= registers( CONV_INTEGER(A1) );
         end if;
 
-        if( CONV_INTEGER(RA2) = 0 ) then
+        if( CONV_INTEGER(A2) = 0 ) then
             RD2 <= X"00000000";
         else
-            RD2 <= mem( CONV_INTEGER(RA2) );
+            RD2 <= registers( CONV_INTEGER(A2) );
         end if;
 	end process;
 end;
