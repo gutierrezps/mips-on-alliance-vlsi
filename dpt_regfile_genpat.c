41cbbad5f981c9f37e1025c05f74e41ad103caa1
Gutierrez PS <gutierrez@live.jp>
2014-12-03 21:37:35 -0300

    Created golden model for regfile; updated testing scenarios

--- dpt_regfile_genpat.c
+++ dpt_regfile_genpat.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include "genpat.h"
+
 #include "include/utils.c"
+#include "include/regfile.c"
 
-int curvect = 0;
 char CLK = 0;
-
-char * cvect() { return inttostr(curvect); }
-
 void toggleClock() {
     CLK = !CLK;
     AFFECT(cvect(), "CLK", inttostr(CLK));
@@ -14,112 +12,103 @@ void toggleClock() {
 
 int main () {
   
-  DEF_GENPAT("dpt_regfile_genpat");
-  
-  // endereços de entrada
-  DECLAR("CLK", ":2", "B", IN , "", "");
+    DEF_GENPAT("dpt_regfile_genpat");
 
-  DECLAR("A3" , ":2", "X", IN , "4 down to 0" , "");
-  DECLAR("WD3", ":2", "X", IN , "31 down to 0", "");
-  DECLAR("WE3", ":2", "B", IN , "", "");
+    // endereços de entrada
+    DECLAR("CLK", ":2", "B", IN , "", "");
 
-  DECLAR("A1" , ":2", "X", IN , "4 down to 0" , "");
-  DECLAR("RD1", ":2", "X", OUT, "31 down to 0", "");
+    DECLAR("A3" , ":2", "X", IN , "4 down to 0" , "");
+    DECLAR("WD3", ":2", "X", IN , "31 down to 0", "");
+    DECLAR("WE3", ":2", "B", IN , "", "");
 
-  DECLAR("A2" , ":2", "X", IN , "4 down to 0" , "");
-  DECLAR("RD2", ":2", "X", OUT, "31 down to 0", "");
-  
-  DECLAR("Vdd", ":2", "B", IN , "", "" );
-  DECLAR("Vss", ":2", "B", IN , "", "" );
-  
-  LABEL ("regfile");
-  
-  AFFECT (cvect(), "Vdd", "0b1");
-  AFFECT (cvect(), "Vss", "0b0");
-  AFFECT (cvect(), "CLK", inttostr(CLK));
-  AFFECT(cvect(), "A1",  "0");
-  AFFECT(cvect(), "A2",  "0");
-  AFFECT(cvect(), "A3",  "0");
-  AFFECT(cvect(), "WD3",  "0");
-  AFFECT(cvect(), "WE3",  "0");
-  
-  curvect++;
-  toggleClock();
-  
-  AFFECT(cvect(), "RD1", "0");
-  AFFECT(cvect(), "RD2", "0");
-  
-  curvect++;
-  toggleClock();
+    DECLAR("A1" , ":2", "X", IN , "4 down to 0" , "");
+    DECLAR("RD1", ":2", "X", OUT, "31 down to 0", "");
 
-  AFFECT(cvect(), "A3",  "0");
-  AFFECT(cvect(), "WE3", "1");
-  AFFECT(cvect(), "WD3", "0xFFFFFFFF");
-  
-  // write on rising edge
-  curvect++;
-  toggleClock();
-  curvect++;
-  toggleClock();
-  
-  // reads on rising edge
-  curvect++;
-  toggleClock();
-  curvect++;
-  toggleClock();
-  
+    DECLAR("A2" , ":2", "X", IN , "4 down to 0" , "");
+    DECLAR("RD2", ":2", "X", OUT, "31 down to 0", "");
 
-  // teste dos demais registradores
-  int reg;
-  
-  for(reg = 1; reg < 32; reg++) {
+    DECLAR("Vdd", ":2", "B", IN , "", "" );
+    DECLAR("Vss", ":2", "B", IN , "", "" );
 
-    AFFECT(cvect(), "A3",  inttostr(reg));
-    AFFECT(cvect(), "WD3", "0xFFFFFFFF");
-    AFFECT(cvect(), "WE3", "1");
-    
-    curvect++;
-    toggleClock();
-    curvect++;
-    toggleClock();
-    
-    AFFECT(cvect(), "A1",  inttostr(reg));
-    AFFECT(cvect(), "A2",  inttostr(reg));
-    
-    curvect++;
-    toggleClock();
-    
-    AFFECT(cvect(), "RD1", "0xFFFFFFFF"); 
-    AFFECT(cvect(), "RD2", "0xFFFFFFFF");
-    
-    curvect++;
-    toggleClock();
-    
-    AFFECT(cvect(), "A3",  inttostr(reg));
-    AFFECT(cvect(), "WD3", "0");
-    AFFECT(cvect(), "WE3", "1");
-    
-    curvect++;
-    toggleClock();
-    curvect++;
-    toggleClock();
-    
+    LABEL ("regfile");
 
-    AFFECT(cvect(), "A1", inttostr(reg));
-    AFFECT(cvect(), "A2", inttostr(reg));
+    AFFECT(cvect(), "Vdd", "0b1");
+    AFFECT(cvect(), "Vss", "0b0");
+    AFFECT(cvect(), "CLK", inttostr(CLK));
+  
+    RegInit();
+    RegAffect();
+    
+    RegA3 = 0;
+    RegWD3 = 0xFFFFFFFF;
+    RegWE3 = 1;
+    RegAffect();
     
     curvect++;
     toggleClock();
-    
-    AFFECT(cvect(), "RD1", "0");
-    AFFECT(cvect(), "RD2", "0");
-    
+    RegWrite();
+
     curvect++;
     toggleClock();
     
-  }  
-  
-  SAV_GENPAT ();
-  
-  return 0;
+    int reg;
+
+    for(reg = 1; reg < 32; reg++) {
+        RegA1 = reg - 1;
+        RegA2 = reg;
+        RegA3 = reg;
+        
+        RegWD3 = 0x00000000;
+        RegWE3 = 1;
+        RegAffect();
+        
+        curvect++; toggleClock();
+        RegWrite();
+        curvect++; toggleClock();
+        
+        RegWD3 = 0xFFFFFFFF;
+        RegWE3 = 0;
+        RegAffect();
+        
+        curvect++; toggleClock();
+        RegWrite();
+        curvect++; toggleClock();
+        
+        RegWE3 = 1;
+        RegAffect();
+        
+        curvect++; toggleClock();
+        RegWrite();
+        curvect++; toggleClock();
+        
+        RegWD3 = 0x00000000;
+        RegWE3 = 0;
+        RegAffect();
+        
+        curvect++; toggleClock();
+        RegWrite();
+        curvect++; toggleClock();
+        
+        RegWE3 = 1;
+        RegAffect();
+        
+        curvect++; toggleClock();
+        RegWrite();
+        curvect++; toggleClock();
+        
+        if(reg % 2 == 1) {
+            RegWD3 = 0xFFFFFFFF;
+            RegWE3 = 1;
+            RegAffect();
+            
+            curvect++; toggleClock();
+            RegWrite();
+            curvect++; toggleClock();
+        }
+        
+    }  
+
+    SAV_GENPAT ();
+
+    return 0;
 }
--- include/regfile.c
+++ include/regfile.c
@@ -1,19 +1,70 @@
 
-int RegFile[32], RegD1, RegD2;
-char RegWE3, RegA1, RegA2, RegInit[32];
+int RegFile[32], RegRD1, RegRD2, RegWD3;
+char RegA3, RegWE3, RegA1, RegA2, RegHasValue[32];
 
 void RegInit() {
     char i;
     
     for(i = 0; i < 32; i++) {
-        RegInit[i] = 0;
+        RegHasValue[i] = 0;
     }
-}
-
-void UpdateStatus() {
+    
+    RegA1 = 0;
+    RegA2 = 0;
+    RegRD1 = 0;
+    RegRD2 = 0;
+    
+    RegWD3 = 0;
+    RegWE3 = 0;
+    RegA3 = 0;
     
 }
 
-void RegWrite(char A3, int WD3) {
+void RegAffect() {
+    if(RegA1 < 0 || RegA1 > 32 || RegA2 < 0 || RegA2 > 32 ||
+        RegA3 < 0 || RegA3 > 32)
+    {
+        fprintf(stderr, "Invalid address value\n");
+        exit(-1);
+    }
+
+    AFFECT(cvect(), "A1",  inttostr(RegA1));
+    AFFECT(cvect(), "A2",  inttostr(RegA2));
     
+    if(RegA1 == 0) {
+        AFFECT(cvect(), "RD1", "0");
+    } else if(RegHasValue[RegA1] == 1) {
+        AFFECT(cvect(), "RD1", inttohex(RegFile[RegA1]));
+    } else {
+        AFFECT(cvect(), "RD1", "0x********");
+    }
+    
+    if(RegA2 == 0) {
+        AFFECT(cvect(), "RD2", "0");
+    } else if(RegHasValue[RegA2] == 1) {
+        AFFECT(cvect(), "RD2", inttohex(RegFile[RegA2]));
+    } else {
+        AFFECT(cvect(), "RD2", "0x********");
+    }
+    
+    AFFECT(cvect(),  "A3",  inttostr(RegA3));
+    AFFECT(cvect(), "WD3",  inttohex(RegWD3));
+    AFFECT(cvect(), "WE3",  inttostr(RegWE3));
+    
+}
+
+
+
+void RegWrite() {
+    if(RegA3 < 0 || RegA3 > 32) {
+        fprintf(stderr, "Invalid address value\n");
+        exit(-1);
+    }
+    
+    if(RegA3 > 0 && RegWE3 == 1) {
+        RegFile[RegA3] = RegWD3 & 0xFFFFFFFF;
+        RegHasValue[RegA3] = 1;
+    }
+    
+    RegAffect();
 }
