4706376dd4036869c767d679d79dca7e5a0eb6e7
Gutierrez PS <gutierrez@live.jp>
2015-03-10 13:09:02 -0300

    Changed 2-input muxes to ternary operator; added Rs, Rt and Rd fields; changed ALUZero logic

--- include/datapath.c
+++ include/datapath.c
@@ -1,124 +1,103 @@
 
 
-char DptOpcode = 0, DptFunct = 0;
+char DptOpcode = 0, DptFunct = 0, DptRs = 0, DptRt = 0, DptRd = 0;
 
 signed int DptRegFile[32] = {0}, DptRegA = 0, DptRegB = 0,
         DptInstr = 0, DptData = 0, DptALUOut = 0, DptPC = 0,
         DptReadData = 0, DptWriteData = 0, DptMemAdr = 0,
         DptALUSrcA = 0, DptALUSrcB = 0, DptALUResult = 0,
-        DptALUZero = 0, DptPCNext = 0;
+        DptALUZero = 0, DptPCNext = 0, DptImm = 0,
+		DptPCJump = 0;
 
-signed int DptImm = 0;
 
-signed int alu(signed int a, signed int b, char ctrl) {
-	signed int res = 0;
+		
+		
+char ALUZero = 0;
+int ALU(int A, int B, char Ctrl) {
+	int Res = 0;
 	
-	switch(ctrl) {
-		case 0b000: res = a & b; break;
-		case 0b001: res = a | b; break;
-		case 0b010: res = a + b; break;
-		case 0b110: res = a - b; break;
-		case 0b111: res = (a < b); break;
+	switch(Ctrl) {
+		case 0b000: Res = A & B;  break;
+		case 0b001: Res = A | B;  break;
+		case 0b010: Res = A + B;  break;
+		case 0b100: Res = A & ~B; break;
+		case 0b101: Res = A | ~B; break;
+		case 0b110: Res = A - B;  break;
+		case 0b111: Res = A < B;  break;
 	}
-    
-    return res;
+	
+	ALUZero = Y == 0;
+	
+	return Y;
 }
 
 void DptUpdateComb() {
 
-    // MemAdr mux
-    if(FsmIorD == 0) {
-        DptMemAdr = DptPC;
-    } else {
-        DptMemAdr = DptALUOut;
-    }
+    DptMemAdr = FsmIorD ? DptALUOut : DptPC;    // MemAdr mux
     
     DptWriteData = DptRegB;
     
-    
-    // Immediate
-    DptImm = DptInstr & 0x0000FFFF;
-    
+    // 16-bit two's complement and sign extending
     if(DptImm > 32767) {
         DptImm = DptImm - 1;
         DptImm = ( ~DptImm ) & 0x0000FFFF;
         DptImm = -DptImm;
     }
     
-    // ALU SrcA mux
-    if(FsmALUSrcA == 0) {
-        DptALUSrcA = DptPC;
-    } else {
-        DptALUSrcA = DptRegA;
-    }
+    DptALUSrcA = FsmALUSrcA ? DptRegA : DptPC;	// ALU SrcA mux
     
-    // ALU SrcB mux
-    switch(FsmALUSrcB) {
+    switch(FsmALUSrcB) {		// ALU SrcB mux
         case 0: DptALUSrcB = DptRegB; break;
         case 1: DptALUSrcB = 4; break;
         case 2: DptALUSrcB = DptImm; break;
-        case 3: DptALUSrcB = DptImm << 2; break;
+        case 3: DptALUSrcB = (DptImm << 2) & 0x0FFFFFFFF; break;
+        default: printf("Error\n");
     }
     
-    // ALU Result
-    DptALUResult = alu(DptALUSrcA, DptALUSrcB, FsmALUControl);
+    DptALUResult = ALU(DptALUSrcA, DptALUSrcB, FsmALUControl);
+    DptALUZero = ALUZero;
     
-    if(DptALUResult == 0) {
-        DptALUZero = 1;
-    } else {
-        DptALUZero = 0;
-    }
+	//                Instr[25:0]                  PC[32:28]
+    DptPCJump = ((DptInstr & 0x3FFFFFF) << 2) | (DptPC & 0xF0000000);
     
-    
-    // PC Next
-    switch(FsmPCSrc) {
+    switch(FsmPCSrc) {				// PC mux
         case 0: DptPCNext = DptALUResult; break;
         case 1: DptPCNext = DptALUOut; break;
-        //case 2: DptPCNext = DptPCJump; break;
+        case 2: DptPCNext = DptPCJump; break;
+        default: printf("Error\n");
     }
-    
 }
 
 
 void DptUpdateSeq() {
     unsigned int data = 0;
+	char addr = 0;
     
-    // Instr reg
-    if(FsmIRWrite == 1) {
+    if(FsmIRWrite) {			// Instr reg
         DptInstr = DptReadData;
-        
-        DptOpcode = ( DptInstr >> 26 ) & 0b0111111;
-        DptFunct  = DptInstr & 0b0111111;
+		DptOpcode = ( DptInstr >> 26 ) & 0x3F;	// 6-bit mask
+		DptFunct  = DptInstr & 0x3F;
+		DptImm = DptInstr & 0x0FFFF;			// 16-bit mask
+		DptRs = (DptInstr >> 21) & 0x1F;		// 5-bit mask
+		DptRt = (DptInstr >> 16) & 0x1F;
+		DptRd = (DptInstr >> 11) & 0x1F;
     }
     
-    // Data reg
-    DptData = DptReadData;
+    // Register File output registers
+    DptRegA = DptRegFile[DptRs];
+    DptRegB = DptRegFile[DptRt];
     
-    
-    // Register File
-    DptRegA = DptRegFile[DecRs];
-    DptRegB = DptRegFile[DecRt];
-    
-    if(FsmRegWrite == 1) {
-        if(FsmMemtoReg == 0) {
-            data = DptALUOut;
-        } else {
-            data = DptData;
-        }
-        
-        if(FsmRegDst == 0) {
-            DptRegFile[DecRt] = data;
-        } else {
-            DptRegFile[DecRd] = data;
-        }
+    if(FsmRegWrite) {
+		data = FsmMemtoReg ? DptData : DptALUOut;
+		addr = FsmRegDst ? DptRd : DptRt;
+		DptRegFile[addr] = data;
     }
+	
+	DptData = DptReadData;		// Data reg
     
-    // ALU Out reg
-    DptALUOut = DptALUResult;
+    DptALUOut = DptALUResult;		// ALU Out reg
     
-    if(FsmPCEn == 1) {
-        DptPC = DptPCNext;
-    }
+    if(FsmPCEn) DptPC = DptPCNext;
 }
 
 void DptAffectAll() {
